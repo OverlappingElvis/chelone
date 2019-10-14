b80b2b86fab21ed3ce38d81370ae97c77ed69b41
Jeremy Sher <OverlappingElvis@gmail.com>
2019-10-13 19:41:27 -0700

    cleanup

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,14 +1,6 @@
-const fs = require(`fs`)
-const program = require(`commander`)
-
 const { Lexer: TurtleLexer } = require(`./lexer`)
 const TurtleParser = require(`./parser`)
 const Turtle = require(`./turtle`)
-
-program.version(`0.0.1`)
-  .option(`-P --program [program]`, `Turtle program as quoted string`, `to knack :length repeat 6 [forward :length right 90 forward :length * 0.6 right 90 forward :length left 120] end penup setxy 0-50 0-150 pendown knack 200`)
-  .option(`-O --output [filename]`, `Output filename [turtle.svg]`, `turtle.svg`)
-  .parse(process.argv)
 
 const parser = new TurtleParser()
 
